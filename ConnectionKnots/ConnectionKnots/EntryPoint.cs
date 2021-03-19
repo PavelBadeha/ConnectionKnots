e3a33707443dde16f55f2122e663771b8e69dc23
Pavel Badzekha <Pavel_Badzekha@epam.com>
2021-03-20 01:38:16 +0300

    Init

---
+++ ConnectionKnots/ConnectionKnots/EntryPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConnectionKnots
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ ConnectionKnots/ConnectionKnots/Generator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectionKnots
+{
+    public static class Generator
+    {
+        public static double PuassonGenerate()
+        {
+            return double.Epsilon;
+        }
+    }
+}
