1d1878d0218c1c43df4bf170c258891a556e5add
Pavel_Badzekha <Pavel_Badzekha@EPBYMINW0348>
2021-03-20 02:51:14 +0300

    added exp and evenly

--- ConnectionKnots/ConnectionKnots/Generator.cs
+++ ConnectionKnots/ConnectionKnots/Generator.cs
@@ -6,9 +6,27 @@ namespace ConnectionKnots
 {
     public static class Generator
     {
+        public static int EvenlyGenerate(int min, int max)
+        {
+            Random random = new Random();
+
+            return random.Next(min, max);
+        }
+
+        public static double ExpGenerate(double lambda)
+        {
+            Random random = new Random();
+            return (-(1 / lambda)) * Math.Log(random.NextDouble());
+        }
+
         public static double PuassonGenerate()
         {
             return double.Epsilon;
+        }
+
+        public static double GaussGenerate(double m, double sigma)
+        {
+
         }
     }
 }
