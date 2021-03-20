d9c7264b18a1b615ff925ad04c66162ae60dc9b3
user <user@WIN-QSM9RO8JED5>
2021-03-20 03:16:12 +0300

    che to dobavil

---
+++ ConnectionKnots/ConnectionKnots/Generator1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConnectionKnots
+{
+    class Generator1
+    {
+        public static int PuassonGenerate(double lambda)
+        {
+            Random rand = new Random();
+            int k = 0;
+            double p = Math.Exp(--lambda);
+            double G = p;
+            double z = rand.NextDouble();
+
+            while (z <= G)
+            {
+                p = p * lambda / (k + 1);
+                G = G + p;
+                k++;
+            }
+
+            return k;
+        }
+
+        public static double ErlangGenerate(double lambda)
+        {
+            for(int )
+            Generator.ExpGenerate(lambda);
+        }
+    }
+}
