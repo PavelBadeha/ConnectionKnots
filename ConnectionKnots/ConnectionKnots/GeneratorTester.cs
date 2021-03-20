5bc5a42b7935a2d7d91afe4920f6730db6b6d3d5
user <user@WIN-QSM9RO8JED5>
2021-03-20 04:12:56 +0300

    change TestEvenly logic

--- ConnectionKnots/ConnectionKnots/GeneratorTester.cs
+++ ConnectionKnots/ConnectionKnots/GeneratorTester.cs
@@ -34,8 +34,42 @@ namespace ConnectionKnots
             }
 
             double expectation = GetExpectation(data);
+            Console.WriteLine($"Равномерное n = 100, M = {expectation}");
 
-            GetDispersion(data, expectation);
+            Console.WriteLine($"D = {GetDispersion(data, expectation)}");
+
+            data = new double[10000];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = Generator.EvenlyGenerate(0, 10);
+            }
+
+            Console.WriteLine($"Вероятность по {K} карманам");
+            foreach(double p in probability)
+            {
+                Console.Write($"{p}   ");
+            }
+            Console.WriteLine();
+
+            probability = new double[K];
+
+            foreach (double z in data)
+            {
+                probability[(int)(z / (1.0d / K))]++;
+            }
+
+            expectation = GetExpectation(data);
+            Console.WriteLine($"Равномерное n = 10000, M = {expectation}");
+
+            Console.WriteLine($"D = {GetDispersion(data, expectation)}");
+
+            Console.WriteLine($"Вероятность по {K} карманам");
+            foreach (double p in probability)
+            {
+                Console.Write($"{p}   ");
+            }
+            Console.WriteLine();
         }
     }
 }
