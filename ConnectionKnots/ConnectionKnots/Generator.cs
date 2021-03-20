d9b26945bc85cd213d371d4a449849ba070a0258
user <user@WIN-QSM9RO8JED5>
2021-03-20 04:19:59 +0300

    Merge Generator and Generator1

--- ConnectionKnots/ConnectionKnots/Generator.cs
+++ ConnectionKnots/ConnectionKnots/Generator.cs
@@ -19,9 +19,33 @@ namespace ConnectionKnots
             return (-(1 / lambda)) * Math.Log(random.NextDouble());
         }
 
-        public static double PuassonGenerate()
+        public static double PuassonGenerate(double lambda)
         {
-            return double.Epsilon;
+            Random rand = new Random();
+            int k = 0;
+            double p = Math.Exp(--lambda);
+            double G = p;
+            double z = rand.NextDouble();
+
+            while (z <= G)
+            {
+                p = p * lambda / (k + 1);
+                G = G + p;
+                k++;
+            }
+
+            return k;
+        }
+
+        public static double ErlangGenerate(double lambda)
+        {
+            double t = 0;
+            for (int i = 1; i <= 3; i++)
+            {
+                t += Generator.ExpGenerate(lambda);
+            }
+
+            return t;
         }
 
         public static double GaussGenerate(double m, double sigma)
--- ConnectionKnots/ConnectionKnots/GeneratorTester.cs
+++ ConnectionKnots/ConnectionKnots/GeneratorTester.cs
@@ -126,5 +126,60 @@ namespace ConnectionKnots
             }
             Console.WriteLine();
         }
+
+        public static void TestEvenly()
+        {
+            double[] data = new double[100];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = Generator.Generate(0, 10);
+            }
+
+            double[] probability = new double[K];
+
+            foreach (double z in data)
+            {
+                probability[(int)(z / (1.0d / K))]++;
+            }
+
+            double expectation = GetExpectation(data);
+            Console.WriteLine($"Равномерное n = 100, M = {expectation}");
+
+            Console.WriteLine($"D = {GetDispersion(data, expectation)}");
+
+            Console.WriteLine($"Вероятность по {K} карманам");
+            foreach (double p in probability)
+            {
+                Console.Write($"{p / data.Length}   ");
+            }
+            Console.WriteLine();
+
+            data = new double[10000];
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = Generator.EvenlyGenerate(0, 10);
+            }
+
+            probability = new double[K];
+
+            foreach (double z in data)
+            {
+                probability[(int)(z / (1.0d / K))]++;
+            }
+
+            expectation = GetExpectation(data);
+            Console.WriteLine($"Равномерное n = 10000, M = {expectation}");
+
+            Console.WriteLine($"D = {GetDispersion(data, expectation)}");
+
+            Console.WriteLine($"Вероятность по {K} карманам");
+            foreach (double p in probability)
+            {
+                Console.Write($"{p / data.Length}   ");
+            }
+            Console.WriteLine();
+        }
     }
 }
